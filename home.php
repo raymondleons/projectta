a57b4506a4edc680d096620402166f6eb8398397
Nur Mala Nisfa <nurmalanisfa6@gmail.com>
2019-01-16 18:33:46 -0800

    nambah berita terbaru

--- home.php
+++ home.php
@@ -6,3 +6,7 @@
 <p>Upaya penanganan penderita autisme khususnya kota Batam telah dilakukan dengan mendirikan Pusat Layanan Autis  pada tanggal 18 Februari 2014 diakses pada tanggal 4 januari 2018. Pusat Layanan ini bertujuan untuk memberikan pendidikan kepada penderita autis agar dapat berkomunikasi dengan baik serta dapat menyesuaikan diri dengan lingkungan sekitarnya. Salah satu kegiatan di Pusat Layanan Autis Batam yaitu terapi.</p>
 <p>Metode terapi yang digunakan yaitu, Terapi Wicara adalah terapi bahasa yang bertujuan untuk meningkatkan kemampuan berbicara penderita. Terapi Okupasi adalah melatih perkembangan motorik yang bertujuan untuk melatih otot penderita agar dapat digunakan dengan benar. Terapi Fisik atau Fisiologi bertujuan untuk memperbaiki keseimbangan tubuhnya yang dikarenakan ototnya kurang kuat.</p>
 </div>
+
+<div class="page-header">
+    <h1 class="entry-title">Berita Terbaru </h1>
+</div>
